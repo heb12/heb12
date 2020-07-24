8d0407f8e8aea95eb67e36d84001084aa71c0e1f
Daniel <brikbusters@gmail.com>
2020-07-24 02:17:01 -0400

    Fix startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 # Run this on start
 mkdir bibles
 wget http://api.heb12.com/translations/json/en/web.json
-node biblec/compiler.js ./web.json web bibles
+node biblec/compiler.js ../web.json web bibles
 cc biblec/biblec.c fbrp/fbrp.c app.c
