ec79e25242c274dd7a62769faaaa8bd8bf1c5c8a
Daniel <brikbusters@gmail.com>
2020-10-07 12:45:06 -0400

    Push old compatibility changes for submodules Also improved CLI a bit.

--- app.c
+++ app.c
@@ -4,88 +4,104 @@
 
 #include "fbrp/reference.h"
 #include "fbrp/fbrp.h"
+#include "biblec/main.h"
 #include "biblec/biblec.h"
 
 struct Translation loadedTranslations[10];
 
 // Simple function to print strings with text break
-void printBreak(char *string, int length, int breakAt) {
-	int nextBreak = breakAt;
-	for (int c = 0; c < length; c++) {
-		printf("%c", string[c]);
-		if (c == nextBreak) {
-			printf("\n");
-			nextBreak = nextBreak * 2;
+void printBreak(char *string, int breakAt) {
+	for (int c = 0; string[c] != '\0'; c++) {
+		putchar(string[c]);
+		if (c > breakAt && string[c] == ' ') {
+			putchar('\n');
+			breakAt *= 2;
 		}
 	}
 }
 
-int main() {
-	printf("%s\n", "@ Heb12Lite CLI v0.1.0");
+void debugPrint(struct Reference *ref) {
+	printf("Book: %s\n", ref->book);
 
-	int tryFile;
+	for (int c = 0; c < ref->chapterLength; c++) {
+		printf("Chapter: %d-%d\n", ref->chapter[c].r[0], ref->chapter[c].r[1]);
+	}
+
+	for (int c = 0; c < ref->verseLength; c++) {
+		printf("Verse: %d-%d\n", ref->verse[c].r[0], ref->verse[c].r[1]);
+	}
+}
+
+
+int main() {
+	printf("%s\n", "@ Heb12Lite");
+
+	int tryFile = 0;
 	parseIndexFile(
 		&tryFile,
 		&loadedTranslations[0],
 		"bibles/web.i",
 		"bibles/web.t"
 	);
-	
+
 	if (tryFile) {
-		printf("%s\n", "! Index loading failure");
+		printf("%s", "! Index loading failure");
 		return 0;
 	} else {
-		printf("%s\n", "@ WEB Bible Loaded");
+		printf("%s", "@ WEB Bible Loaded");
 	}
-	
+
 	while (1) {
-		printf(": ");
-		char input[20];
-		fgets(input, 20, stdin);
-		
+		printf("\n: ");
+		char input[50];
+		fgets(input, 50, stdin);
+
 		if (input[0] == '?') {
-			printf("%s\n", "@ Type a reference to see the verses");
-			printf("%s\n", "@ Type \"+<number>\" to see the next verse");
+			printf("\n%s\n", "@ Type a reference to see the verses");
 			continue;
 		} else if (input[0] == 'x') {
 			return 0;
 		}
-		
+
+
 		int *tryRef;
-		struct Reference ref;
-		parseReference(tryRef, input, &ref);
-		
-		for (int r = 0; r < ref.verseX; r++) {
-			int tryGet = 0;
-			char result[30][600];
-			
-			// Change to if range end and start is different
-			int to = 1;
-			if (ref.verse[r].r[0] != ref.verse[r].r[1]) {
-				to = ref.verse[r].r[1] - ref.verse[r].r[0] + 1;
+		struct Reference ref = parseReference(tryRef, input);
+
+		int verses = ref.verseLength;
+		if (verses == 0 && ref.chapterLength == 1) {verses = 1;}
+
+		for (int r = 0; r < verses; r++) {
+			int verseStart = 1;
+			int verseEnd = 0;
+			if (ref.verseLength != 0) {
+				verseStart = ref.verse[r].r[0];
+				verseEnd = ref.verse[r].r[1];
 			}
-			
-			getVerses(
-				&tryGet,
-				result,
-				loadedTranslations[0],
+
+			int tryReader;
+			struct Reader reader = reader_new(
+				&tryReader,
+				&loadedTranslations[0],
 				ref.book,
 				ref.chapter[0].r[0],
-				ref.verse[r].r[0],
-				to
+				verseStart,
+				verseEnd
 			);
-			
-			if (tryGet) {
-				printf("%s%d\n", "! Error getting verse: ", tryGet);
-				return tryGet;
+
+			if (tryReader) {
+				printf("Verse error = %d", tryReader);
+				return 0;
 			}
-			
-			for (int i = 0; i < to; i++) {
-				printf("\n%d. ", (ref.verse[r].r[0] + i));
-				printBreak(result[i], strlen(result[i]), 70);
+
+			while (1) {
+				if (reader_next(&reader)) {break;}
+				putchar('\n');
+				printf("%d. ", verseStart + reader.linesRead - 1);
+				printBreak(reader.result, 70);
+				putchar('\n');
 			}
-			
-			printf("\n\n");
+
+			fclose(reader.file);
 		}
 	}
 }
