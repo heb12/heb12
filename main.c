996491b6604f6ae3ca1aa7ac58ddc18ba0280764
Daniel Cook <brikbusters@gmail.com>
2021-07-03 19:38:56 -0400

    update with dependencies

--- main.c
+++ main.c
@@ -13,8 +13,8 @@
 char *defaultIndex = "bibles/web.i";
 char *defaultReference = "John 3 16";
 
-struct Biblec_translation translation;
-struct Reference ref;
+struct BiblecTranslation translation;
+struct FbrpReference ref;
 
 // Simple function to print strings with text break
 void printBreak(char *string, int limit) {
@@ -49,7 +49,7 @@ void printError(int type) {
 }
 
 int printVerses(char *input, int fancyPrint) {
-	parseReference(&ref, input);
+	fbrp_parse(&ref, input);
 
 	// Get standard OSIS from whatever
 	// user gave
@@ -71,7 +71,7 @@ int printVerses(char *input, int fancyPrint) {
 			verseEnd = ref.verse[r].range[1];
 		}
 
-		struct Biblec_reader reader;
+		struct BiblecReader reader;
 		int tryReader = biblec_new(
 			&reader,
 			&translation,
