091cde61f7af662209f7c517d58df93de37c875c
Daniel <brikbusters@gmail.com>
2021-10-25 19:00:11 -0400

    Forgot to set executable

--- prebuilt.sh
+++ prebuilt.sh
@@ -8,3 +8,4 @@ curl https://api.heb12.com/translations/biblec/web.i > web.i
 curl https://api.heb12.com/translations/biblec/web.t > web.t
 
 sudo curl -k http://api.heb12.com/static-heb12-x86_64 -o /bin/heb12
+sudo chmod +x /bin/heb12
