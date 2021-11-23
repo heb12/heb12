122b6415aa4236778d4f012f757f4de456c7e32a
Daniel <brikbusters@gmail.com>
2021-11-23 16:13:58 -0500

    Fix prebuilt script issues

--- prebuilt.sh
+++ prebuilt.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-mkdir ~/.local/share/heb12
-mkdir ~/.local/share/heb12/app
+mkdir -p ~/.local/share/heb12/app
 cd ~/.local/share/heb12/
 
 curl https://api.heb12.com/translations/biblec/web.i > web.i
 curl https://api.heb12.com/translations/biblec/web.t > web.t
 
-sudo curl -k http://api.heb12.com/static-heb12-x86_64 -o /bin/heb12
+sudo curl -k https://api.heb12.com/static-heb12-x86_64 -o /bin/heb12
 sudo chmod +x /bin/heb12
