793fab9136619cd52710bf34871cfba79218a704
Daniel C <brikbusters@gmail.com>
2022-01-17 12:56:18 -0500

    More fixes in the install script

--- install.sh
+++ install.sh
@@ -3,26 +3,23 @@
 # This compiles and installs Heb12 CLI on
 # typical Linux based systems.
 
-mkdir ~/.local
-mkdir ~/.local/share
+set -e
 
 echo "Making sure make, curl, git, and gcc are installed.."
 sudo apt install curl git gcc
 
-mkdir ~/.local/share/heb12
-mkdir ~/.local/share/heb12/app
+mkdir -p ~/.local/share/heb12/app
 cd ~/.local/share/heb12/
-git clone --recurse-submodules https://code.heb12.com/heb12/heb12cli app
+git clone --recurse-submodules https://github.com/heb12/cli app
 
 echo "Downloading WEB Bible.."
-curl -L https://api.heb12.com/translations/biblec/web.i > web.i
-curl -L https://api.heb12.com/translations/biblec/web.t > web.t
+curl -ipv4 -L https://api.heb12.com/translations/biblec/web.i > web.i
+curl -ipv4 -L https://api.heb12.com/translations/biblec/web.t > web.t
 
 echo "Compiling app"
 cd app
 make CC=gcc
-cp heb12 /bin/
+chmod +x heb12
 
-cd
-echo "Done."
-heb12 -r "John 3 16"
+echo "Done. Will copy 'heb12' to /usr/bin as sudo."
+sudo cp ~/.local/share/heb12/app/heb12 /usrbin/
