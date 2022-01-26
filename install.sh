0396e9d112475d76badedb03e5b922dad4e03994
Daniel C <brikbusters@gmail.com>
2022-01-26 18:07:09 -0500

    install scripts to copy to /bin

--- install.sh
+++ install.sh
@@ -5,21 +5,21 @@
 
 set -e
 
-echo "Making sure make, curl, git, and gcc are installed.."
+echo "Making sure make, curl, git, and gcc are installed..."
 sudo apt install curl git gcc
 
 mkdir -p ~/.local/share/heb12/app
 cd ~/.local/share/heb12/
 git clone --recurse-submodules https://github.com/heb12/cli app
 
-echo "Downloading WEB Bible.."
+echo "Downloading WEB Bible..."
 curl -ipv4 -L https://api.heb12.com/translations/biblec/web.i > web.i
 curl -ipv4 -L https://api.heb12.com/translations/biblec/web.t > web.t
 
-echo "Compiling app"
+echo "Compiling app..."
 cd app
 make CC=gcc
+chmod +x heb12
 
-echo "Done. Will copy 'heb12' to /usr/bin as sudo."
-sudo cp ~/.local/share/heb12/app/heb12 /usr/bin/
-sudo chmod +x /usr/bin/heb12
+echo "Done. Will copy 'heb12' to /bin/ as sudo."
+sudo cp ~/.local/share/heb12/app/heb12 /bin/
--- prebuilt.sh
+++ prebuilt.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 echo "Making folders"
-mkdir -p ~/.local/share/heb12
-mkdir -p ~/.local/share/heb12/app
+mkdir -p ~/.local/share/heb12/
 cd ~/.local/share/heb12/
 
 echo "Downloading Web (biblec)"
 curl -L https://api.heb12.com/translations/biblec/web.i > web.i
 curl -L https://api.heb12.com/translations/biblec/web.t > web.t
 
-echo "--- Will need superuser to write to /bin/heb12 ---"
+echo "--- Will need superuser to write to /usr/bin/heb12 ---"
 if (uname -a | grep "x86_64"); then
 	sudo curl -L "https://github.com/heb12/cli/releases/download/0.1.2/static-heb12-x86_64" -o /bin/heb12
+	sudo chmod +x /bin/heb12
 	echo "/bin/heb12 Installed"
 else
 	echo "Sorry, only have x86_64 binary for now."
